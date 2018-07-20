44a11b1dfcec2ab66ce9d321acd5cbfe285d4689
Minh_Vnit <hungquocminh1989@gmail.com>
2018-07-20 18:47:35 +0700

    remove meta

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,13 @@
 <?php 
 define('SYSTEM_BASE_URL', 'http://127.0.0.1/brave_src/');
 define('SYSTEM_URL_DOWNLOAD', 'https://laptop-updates.brave.com/download/QUC179');
-define('DATABASE_TYPE', 'pgsql');
+define('SYSTEM_SCRIPT_GOOGLE_ANALYZE', '
+
+');
+/*define('DATABASE_TYPE', 'pgsql');
 define('DATABASE_NAME', 'sdh_2018');
 define('DATABASE_HOST', 'localhost');
 define('DATABASE_USER', 'postgres');
 define('DATABASE_PASS', '123456');
 define('DATABASE_PORT', '5433');
-define('DATABASE_DNS', sprintf ('%1$s:dbname=%2$s;host=%3$s;port=%4$s',DATABASE_TYPE, DATABASE_NAME, DATABASE_HOST, DATABASE_PORT));
+define('DATABASE_DNS', sprintf ('%1$s:dbname=%2$s;host=%3$s;port=%4$s',DATABASE_TYPE, DATABASE_NAME, DATABASE_HOST, DATABASE_PORT));*/
